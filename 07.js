4afd1673855ea9010a98d5be44d958f15ce4cfa4
Erik Sundahl <esundahl@gmail.com>
2013-10-05 15:16:00 -0700

    adds solution for 07

---
+++ 07.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+/**
+ * Dependencies
+ */
+
+var http = require('http');
+
+http.get(process.argv[2], function (res) {
+  res.setEncoding('utf8');
+  res.on('data', console.log);
+  res.on('error', console.error);
+});
