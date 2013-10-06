80287d77e1d279f3614b1f7b13287fc503121605
Erik Sundahl <esundahl@gmail.com>
2013-10-05 17:53:59 -0700

    adds solution for 09

---
+++ 09.js
@@ -0,0 +1,31 @@
+'use strict'
+
+
+/**
+ * Dependencies
+ */
+
+var http = require('http');
+
+recurse(process.argv[2], function () {
+  recurse(process.argv[3], function () {
+    recurse(process.argv[4]);
+  });
+});
+
+
+function recurse (url, cb) {
+ http.get(url, function (res) {
+  var fullText = '';
+  res.setEncoding('utf8');
+  res
+  .on('data', function (data){
+    fullText += data;
+  })
+  .on('end', function () {
+    console.log(fullText);
+    if (cb) cb();
+  });
+ });
+}
+
