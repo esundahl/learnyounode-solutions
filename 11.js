c69c4833cca95b1c9f860c1a801c6aa4f9a4bdb2
Erik Sundahl <esundahl@gmail.com>
2013-10-05 21:04:49 -0700

    adds solution for 11

---
+++ 11.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+/**
+ * Dependencies
+ */
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+  fs.createReadStream(process.argv[2]).pipe(res);
+});
+server.listen(8000);
